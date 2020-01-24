65099cd09e17caa8d80e26d5bd730d01c33a2f80
Matt Wilson <matt@firstflightagency.com>
2020-01-24 17:38:23 -0500

    changed css around a bit, still not working on initial play but works fine after

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -3,7 +3,7 @@ import "./Header.css";
 
 const Header = props => (
   <div className="header">
-    <div className="title">{props.children}</div>
+    <div className="title">{props.children}: Click an image, score a point. Don't click the same one twice!</div>
     <div className="scores">
       Score: {props.score} Highscore: {props.highscore}
     </div>
