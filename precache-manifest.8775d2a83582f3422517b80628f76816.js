90391f2ecd1f2cde587004b25fae2dabd6ba6cf1
Matt Wilson <matt@firstflightagency.com>
2020-01-25 11:28:42 -0500

    Updates

---
+++ precache-manifest.8775d2a83582f3422517b80628f76816.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "189c42a53d41cc82c61090ef37423cd5",
+    "url": "/clickygame2/index.html"
+  },
+  {
+    "revision": "b8006cddbcdc29648fe5",
+    "url": "/clickygame2/static/css/main.ed70248f.chunk.css"
+  },
+  {
+    "revision": "100a5b8886bb94324ca5",
+    "url": "/clickygame2/static/js/2.e9952ba0.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/clickygame2/static/js/2.e9952ba0.chunk.js.LICENSE"
+  },
+  {
+    "revision": "b8006cddbcdc29648fe5",
+    "url": "/clickygame2/static/js/main.9d453743.chunk.js"
+  },
+  {
+    "revision": "e0293ca2a20694660789",
+    "url": "/clickygame2/static/js/runtime-main.eee6ca2c.js"
+  }
+]);
